e5032cd56174126ab96a478439105223b01dbe99
honginpyo1 <31614952+honginpyo1@users.noreply.github.com>
2017-11-29 15:25:47 +0900

    Update linux_vm_completed.sh

--- Desktop/linux_vm_completed.sh
+++ Desktop/linux_vm_completed.sh
@@ -1,3 +1,3 @@
-curl -X POST -H 'Content-type: application/json' \ 
---data '{"text":"VM 생성이 완료되었습니다."}'\ 
+curl -X POST -H 'Content-type: application/json' \
+--data '{"text":"VM 생성이 완료되었습니다."}' \
 https://hooks.slack.com/services/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
